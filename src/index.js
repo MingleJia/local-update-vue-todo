576add56104982d759ee8e92c37a1e215a450833
MingleJia <18767135620@163.com>
2018-11-19 20:52:45 +0800

    注释详解

--- src/index.js
+++ src/index.js
@@ -8,5 +8,5 @@ const root = document.createElement('div');
 document.body.appendChild(root);
 
 new Vue({
-    render: (h) => h(App)
+    render: (h) => h(App) // h是vue中createElement函数,是用来生成HTML DOM元素,这个函数的作用就是生成一个VNode节点，render函数得到这个VNode节点之后，返回给Vue.js 的 mount 函数，渲染成真实 DOM 节点，并挂载到根节点上。
 }).$mount(root);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
 // 使用绝对路径
-const path = require('path');
-// const HTMLPlugin = require('html-webpack-plugin'); 
+const path = require('path'); // path 是webpack基本包
 const webpack = require('webpack');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin'); //引入插件
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //引入插件
 const isDev = process.env.NODE_ENV === 'development';
 const config = {
     target:"web",
@@ -11,16 +10,15 @@ const config = {
     entry:path.join(__dirname,'src/index.js'),
     output:{
         filename: 'bundle.js',
-        path:path.join(__dirname, 'dist'),
+        path:path.join(__dirname, 'dist'), // __dirname表示当前文件所在路径c
     },
     plugins: [
         // make sure to include the plugin for the magic
-        new webpack.DefinePlugin({
+        new webpack.DefinePlugin({ // 使用vue框架时 必须要用这个插件
             'process.env': {
                 NODE_ENV:isDev?'"development"':'"production"', // 注意单引号内的双引号 
             }
         }),// webpack 环境区分打包，选择不同的源代码进行打包 开发版本中用到错误提示 --加大文件大小，其次，降低代码运行效率
-        // new HTMLPlugin(), 
         new VueLoaderPlugin(),
         new HtmlWebpackPlugin()
     ],
@@ -34,10 +32,10 @@ const config = {
                 test:/\.css$/,
                 use:['style-loader','css-loader'],
                 // css-loader：读取css文件内容
-                // style-loader：使用<style>将css-loader内部样式注入到我们的HTML页面
+                // style-loader：使用<style></style>将css-loader内部样式注入到我们的HTML页面
             },
             {
-                test:/\.styl$/,
+                test:/\.styl$/, // 安装stylus-loader stylus
                 use:[
                     'style-loader',
                     'css-loader',
@@ -64,15 +62,16 @@ const config = {
 
 if(isDev) {
     config.devtool = '#cheap-module-eval-source-map',
-    config.devServer = {
+    config.devServer = { // 启动服务
         // 关于webpack devserver的东西都在此配置
         port:8000,
         host:'0.0.0.0',
-        // localhost 127.0.0.1 或者本机IP进行访问（可以再他人或者手机访问）
+        // 可以localhost 127.0.0.1 或者本机内网IP进行访问（可以再他人或者手机访问）如果设置成localhost，在别人电脑上是访问不了的
         overlay: {
             errors:true,// webpack编译错误显示在网页中
         },
-        hot:true, // 页面不刷新 仅更新组件数据
+        hot:true, // 页面不刷新 仅更新组件数据 webpack会自动添加 HMR 插件。所以模块热更新最终还是 HMR 这个插件起的作用。
+
         // open:true,
         // historyFallback: {
         //     // 设置默认入口地址
